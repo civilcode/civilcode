06b6cb9be319f5028fd23a8f260371c5c8a28b07
Nicholas Henry <nicholas.henry@gmail.com>
2019-05-17 13:29:01 -0400

    docs: Add notes for ApplicationService

--- lib/civilcode/application_service.ex
+++ lib/civilcode/application_service.ex
@@ -1,5 +1,13 @@
 defmodule CivilCode.ApplicationService do
-  @moduledoc false
+  @moduledoc """
+  * the "write" side
+  * responsible for processing business use cases
+  * contain functions reflecting commands to operate on an aggregate
+  * typically following the pattern of `impure` -&gt; `pure` -&gt; `impure` function calls
+  * e.g. `read from the repo` -&gt; `call domain logic` -&gt; `write to the repo`
+  * `Service` modules hydrate domain concepts from a repo, call a function\(s\) to perform domain logic and then write the "modified" domain concept to the database.
+  * Top level/public `Service` functions should be only a pipeline \(either `|>` or `with`\) providing a high-level overview of the processing steps.
+  """
 
   defmacro __using__(_) do
   end
