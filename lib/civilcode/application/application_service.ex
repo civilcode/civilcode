607028a2458b0f61cf4a27d2ed5f51afa1b2e55c
Nicholas Henry <nicholas.henry@gmail.com>
2019-05-17 15:20:35 -0400

    Add notes to Application Service

--- lib/civilcode/application/application_service.ex
+++ lib/civilcode/application/application_service.ex
@@ -2,7 +2,9 @@ defmodule CivilCode.ApplicationService do
   @moduledoc """
   * the "write" side
   * responsible for processing business use cases
+  * entry point into the application, receive primitives
   * contain functions reflecting commands to operate on an aggregate
+  * validate at the boundaries
   * typically following the pattern of `impure` -&gt; `pure` -&gt; `impure` function calls
   * e.g. `read from the repo` -&gt; `call domain logic` -&gt; `write to the repo`
   * `Service` modules hydrate domain concepts from a repo, call a function\(s\) to perform domain logic and then write the "modified" domain concept to the database.
