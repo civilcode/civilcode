24de72c2b8090493b331b8af40f3ee39fd06de76
Nicholas Henry <nicholas.henry@gmail.com>
2019-05-17 15:20:53 -0400

    A Process Manager building block

---
+++ lib/civilcode/application/process_manager.ex
@@ -0,0 +1,12 @@
+defmodule CivilCode.DomainService do
+  @moduledoc """
+  > Consider a Process Manager as a sort of Application Service on top of the other components.
+  It can contains one or more stateful aggregate that keep the state of a long running process
+  depending on requirements.
+
+  http://www.dinuzzo.co.uk/2017/04/28/domain-driven-design-event-sourcing-and-micro-services-explained-for-developers/
+  """
+
+  defmacro __using__(_) do
+  end
+end
