643e8a8afb8478b413e7e49e62f8a089f56722af
Nicholas Henry <nicholas.henry@gmail.com>
2019-10-25 10:30:24 -0400

    feat: Allow override of inspect protocol

--- test/support/value_objects.ex
+++ test/support/value_objects.ex
@@ -2,4 +2,8 @@ defmodule Fixtures do
   defmodule Decimal do
     use CivilCode.ValueObject, type: :decimal
   end
+
+  defmodule NaiveMoney do
+    use CivilCode.ValueObject, type: :naive_money
+  end
 end
